5325e270de276b3887befb5fc775697b5547b701
Kevin McPhillips <github@kevinmcphillips.ca>
2011-05-15 18:19:58 -0500

    Added a blacklist of words to avoid. Mostly profanity. Scandalous.

--- lib/memorable_password.rb
+++ lib/memorable_password.rb
@@ -1,4 +1,4 @@
-require 'lib/memorable_password/sample'
+require 'memorable_password/sample'
 
 module MemorablePassword
   
@@ -12,8 +12,9 @@ module MemorablePassword
     :special_characters => false
   }
   
-  class << self; attr_accessor :dictionary end
+  class << self; attr_accessor :dictionary, :blacklist end
   @dictionary = nil
+  @blacklist = nil
     
   def self.generate(length=DEFAULT_LENGTH, opts={})
     dict = initialize_dictionary
@@ -52,6 +53,13 @@ module MemorablePassword
   def self.initialize_dictionary
     unless self.dictionary
       self.dictionary = {}
+      self.blacklist = []
+      
+      # Load blacklist from text file
+      File.foreach(File.join(File.dirname(__FILE__), 'memorable_password', 'blacklist.txt'))do |word|
+        word = word.strip.downcase
+        self.blacklist << word if word =~ /^[a-z]+$/
+      end
       
       # Load system dictionary words
       File.foreach("/usr/share/dict/words"){|word| add_word word}
@@ -67,7 +75,7 @@ module MemorablePassword
     word = word.strip.downcase
     length = word.length
     
-    if length <= MAX_WORD_LENGTH && length > 1
+    if length <= MAX_WORD_LENGTH && length > 1 && word =~ /^[a-z]+$/ && !self.blacklist.include?(word)
       self.dictionary[length] = [] unless self.dictionary[length]
       self.dictionary[length] << word
     end
